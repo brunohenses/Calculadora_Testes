0e0c4a87ca62a5a38fc6291d8f2a9283c6836905
Bruno Henses <henses94@gmail.com>
2025-07-24 11:45:25 +0100

    Adicionados testes de operações básicas

--- testes_main_alunos.py
+++ testes_main_alunos.py
@@ -8,18 +8,38 @@ class TestCalculadora(unittest.TestCase):
     def teste_operacoes_basicas(self):
         # Teste operações básicas de cada operador + - * / % ^
         self.assertEqual(calculadora(2, 3, '+'), 5)
-
+        self.assertEqual(calculadora(3, 2, '-'), 1)
+        self.assertEqual(calculadora(2, 3, '*'), 6)
+        self.assertEqual(calculadora(6, 2, '/'), 3)
+        self.assertEqual(calculadora(7, 2, '%'), 1)
+        self.assertEqual(calculadora(2, 3, '^'), 5)
+        
     def teste_v2_operacoes(self):
         # Teste operações básicas de cada operador + - * / % ^
         self.assertEqual(calculadora_v2(2, 3, '+'), 5)
+        self.assertEqual(calculadora_v2(3, 2, '-'), 1)
+        self.assertEqual(calculadora_v2(2, 3, '*'), 6)
+        self.assertEqual(calculadora_v2(6, 2, '/'), 3)
+        self.assertEqual(calculadora_v2(7, 2, '%'), 1)
+        self.assertEqual(calculadora_v2(2, 3, '^'), 5)
 
     def teste_v3_operacoes(self):
         # Teste operações básicas de cada operador + - * / % ^
         self.assertEqual(calculadora_v3(2, 3, '+'), 5)
+        self.assertEqual(calculadora_v3(3, 2, '-'), 1)
+        self.assertEqual(calculadora_v3(2, 3, '*'), 6)
+        self.assertEqual(calculadora_v3(6, 2, '/'), 3)
+        self.assertEqual(calculadora_v3(7, 2, '%'), 1)
+        self.assertEqual(calculadora_v3(2, 3, '^'), 5)
 
     def teste_v4_operacoes(self):
         # Teste operações básicas de cada operador + - * / % ^
         self.assertEqual(calculadora_v4(2, 3, '+'), 5)
+        self.assertEqual(calculadora_v4(3, 2, '-'), 1)
+        self.assertEqual(calculadora_v4(2, 3, '*'), 6)
+        self.assertEqual(calculadora_v4(6, 2, '/'), 3)
+        self.assertEqual(calculadora_v4(7, 2, '%'), 1)
+        self.assertEqual(calculadora_v4(2, 3, '^'), 5)
 
     def teste_operacoes_diversas(self):
         # Teste divisão por zero operador para todas versões / %
